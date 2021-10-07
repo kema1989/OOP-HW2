d601ba80fa151660c607729c8bc44ed2cb2411fa
kema1989 <86516029+kema1989@users.noreply.github.com>
2021-10-07 13:15:32 +0600

    Update Helicopter.cs
    чвапмирош

--- OOP-HW2/Helicopter.cs
+++ OOP-HW2/Helicopter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,11 +7,10 @@ using System.Threading;
 
 namespace OOP_HW2
 {
-    class Helicopter
+    class Helicopter : IDisposable
     {
         private Type type;
         private int ownerAge;
-        private int weightLift;
         private int fuel;
         private int mileage;
         private int price;//тыщ долларов США
@@ -20,7 +19,7 @@ namespace OOP_HW2
             get { return ownerAge; }
             private set
             {
-                if(value < 21 && value > 60)
+                if (value < 21 && value > 60)
                 {
                     Console.WriteLine("Летать на вертолете могут только люди от 21 до 60 лет");
                 }
@@ -45,20 +44,10 @@ namespace OOP_HW2
                 }
             }
         }
+        
 
-        private int WeightLift
-        {
-            get { return weightLift; }
-            set
-            {
-
-            }
-        }
-
-        public Helicopter(string destination)
-        {
-            
-        }
+        public int Mileage { get; set; }
+        
 
         public Helicopter(Type type)
         {
@@ -81,69 +70,141 @@ namespace OOP_HW2
                     break;
             }
         }
-        public virtual void GetFuel(int fuel)
+        public virtual void GetFuel()
         {
+            int fuel = Convert.ToInt32(Console.ReadLine());
             this.Fuel += fuel;
             Console.WriteLine($"Вы заправились на {fuel} литров");
         }
 
-        public virtual void TravelTo(Destinations destination)
+        public virtual void TravelTo()
         {
-            Console.WriteLine("");
-            switch (destination)
+            Console.WriteLine("Полететь в Чолпон-Ату - 1, в Алмату - 2, в Москву - 3, в Париж - 4");
+            Console.WriteLine("Круто, ожидайте...");
+            switch (Console.ReadLine())
             {
-                case Destinations.CholponAta:
+                case "1":
                     mileage += 250;
                     fuel -= 50;
                     Thread.Sleep(2000);
                     break;
-                case Destinations.Almaty:
+                case "2":
                     mileage += 400;
                     fuel -= 80;
                     Thread.Sleep(3000);
                     break;
-                case Destinations.Moscow:
+                case "3":
                     mileage += 3000;
                     fuel -= 600;
                     Thread.Sleep(6000);
                     break;
-                case Destinations.Paris:
+                case "4":
                     mileage += 5000;
                     fuel -= 1000;
                     Thread.Sleep(9000);
                     break;
             }
 
-            if(fuel < 0)
+            if (fuel < 0)
             {
                 Console.WriteLine("Вы неправильно расчитали топливо, и поэтому не смогли долететь и разбились.\nВ следующей жизни будьте осторожнее");
                 fuel += 1000;
             }
-            if(mileage > 20000)
+            if (mileage > 20000)
             {
                 Console.WriteLine("Ваш вертолет устарел ");
             }
+            Console.WriteLine("Прилетели...");
         }
 
         public void Repair()
         {
+            mileage -= 500;
+            price += 2000;
+        }
 
+        public void Repair(int mileage)
+        {
+            this.mileage -= mileage;
+            price += mileage * 4;
+            fuel += 500;
         }
 
         public void Sell(int yourPrice)
         {
-
+            if(yourPrice > price)
+            {
+                Console.WriteLine("Вы ждали 100 лет и все равно не продали свою фигню");
+            }
+            else if(yourPrice < price && yourPrice > 0.6 * price)
+            {
+                Thread.Sleep(5000);
+                Console.WriteLine($"Вы продали свой вертолет за {yourPrice} долларов");
+            }
+            else
+            {
+                Console.WriteLine($"Вы продали свой вертолет за {yourPrice} долларов");
+            }
         }
 
+        public void Start()
+        {
+            Console.WriteLine("Путешествовать - 1, заправиться - 2, ремонт - 3, продвинутый ремонт - 4, продать вертолет - 5");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    TravelTo();
+                    Start();
+                    break;
+                case "2":
+                    GetFuel();
+                    Start();
+                    break;
+                case "3":
+                    Repair();
+                    Start();
+                    break;
+                case "4":
+                    Console.WriteLine("Введите кол-во уменьшаемого пробега");
+                    int i = Convert.ToInt32(Console.ReadLine());
+                    Repair(i);
+                    Start();
+                    break;
+                case "5":
+                    Console.WriteLine("Введите желаемую стоимость");
+                    int j = Convert.ToInt32(Console.ReadLine());
+                    Sell(j);
+                    Start();
+                    break;
+                default:
+                    Console.WriteLine("До свидания");
+                    break;
+            }
+        }
+
+        public static void Greetings()
+        {
+            Console.WriteLine("Хелло, как дела?");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Deleted!");
+        }
+
+        ~Helicopter()
+        {
+            Console.WriteLine("Deleted!!!");
+        }
     }
 
-    public enum Destinations
-    {
-        CholponAta,
-        Almaty,
-        Moscow,
-        Paris
-    }
+    //public enum Destinations
+    //{
+    //    CholponAta,
+    //    Almaty,
+    //    Moscow,
+    //    Paris
+    //}
 
     public enum Type
     {
